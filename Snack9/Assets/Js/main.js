1eea6d33f1cbaae04ec5d9c10608b59bab36fc58
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-18 19:15:19 +0200

    complete details html and js file Snack9
    aggiunto ciclo for

--- Snack9/Assets/Js/main.js
+++ Snack9/Assets/Js/main.js
@@ -4,3 +4,23 @@
 -for
 -querySelector
 */
+
+// inizializzazione delle varibili
+let somma = 0;
+let media = 0;
+
+// ciclo for
+for (let i = 0; i <= 10; i++) {
+    let singleNumber = i;
+    /* console.log(singleNumber); */
+    
+    somma = somma + singleNumber;
+    /* console.log(somma); */
+    media = somma / 11;
+/*     console.log(media);    */
+    
+}
+
+// stampa a schermo
+document.querySelector(".somma").innerHTML = `la somma dei primi 10 numeri è: ${somma}`;
+document.querySelector(".media").innerHTML = `la media dei primi 10 numeri è: ${media}`;
