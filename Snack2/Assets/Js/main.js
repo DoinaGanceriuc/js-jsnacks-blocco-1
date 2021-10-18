4b82504d7a8f21b6274c4f10d8ace865ffde0cd9
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-18 15:55:11 +0200

    complete details js file
    aggiunto prompt e condizione di verifca

--- Snack2/Assets/Js/main.js
+++ Snack2/Assets/Js/main.js
@@ -3,5 +3,23 @@
 /* Strumenti
 -prompt
 -if/else
--querySelector
+-querySelector/getElementById
 */
+
+//inizializzazione variabili
+const wordOne = prompt("Inserisci una parola");
+const wordTwo = prompt("Inserisci un'altra parola");
+
+
+// condizione di verifica
+if (wordOne.length < wordTwo.length) {
+    /* let message = `la parola ${wordOne} è più corta della parola ${wordTwo}`; */
+    document.getElementById("one").innerHTML = `"${wordOne}" è la parola più corta`;
+    
+} else if (wordOne.length > wordTwo.length) {
+    document.getElementById("one").innerHTML = `"${wordOne}" è la parola più lunga`;
+   
+} else {
+    document.getElementById("one").innerHTML = `le due parole digitate hanno la stessa lunghezza`;
+  
+}
