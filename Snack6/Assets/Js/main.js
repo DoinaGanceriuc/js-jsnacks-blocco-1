9a0f40f4a2f77ba1824306cee5b4274131d49fff
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-18 17:39:21 +0200

    complete details js file Snack6
    aggiunto prompt e condizionale

--- Snack6/Assets/Js/main.js
+++ Snack6/Assets/Js/main.js
@@ -6,3 +6,17 @@
 -Math
 */
 
+// inizializzazione delle variabili
+const number = Number(prompt("Inserisci un numero"));
+
+let cuboNumber = Math.pow(number, 3);
+/* console.log(cuboNumber); */
+
+// condizione di verifica
+if (isNaN(number)){
+     let message = `si prega di indicare un numero corretto`;
+    document.querySelector("h1").innerHTML = message;
+
+} else {
+    document.querySelector("h1").innerHTML = `il cubo di ${number} è ${cuboNumber}`;
+}
