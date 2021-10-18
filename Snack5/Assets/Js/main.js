f9dea436ab79085fdf1d134d10d1bd190961c47b
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-18 17:04:51 +0200

    Add basic structure html and js file folder Snack5

---
+++ Snack5/Assets/Js/main.js
@@ -0,0 +1,10 @@
+/* Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.. */
+
+/* Strumenti
+-prompt
+-for
+-if/else
+-querySelector/getElementById
+*/
