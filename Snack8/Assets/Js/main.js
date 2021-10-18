e66ce2420913ac632cc95109601119b52c7c7f93
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-18 19:11:38 +0200

    complete details js file Snack8
    aggiunto prompt e ciclo for

--- Snack8/Assets/Js/main.js
+++ Snack8/Assets/Js/main.js
@@ -4,4 +4,23 @@ e calcola la somma di tutte le cifre che compongono il numero. */
 /* Strumenti
 -prompt
 -querySelector
+-for
 */
+
+// inizializzazione delle variabili
+let number = prompt("Inserisci un numero di 4 cifre");
+
+let somma = 0;
+
+// ciclo for
+for (let index = 0; index < number.length; index++) {
+    const element = Number(number[index]);
+ /*    console.log(element); */
+    somma = somma + element;
+      
+    
+}
+/* console.log(somma); */
+
+// stampa a schermo
+document.querySelector("h1").innerHTML = `la somma dei numeri digitati è: ${somma}`;
