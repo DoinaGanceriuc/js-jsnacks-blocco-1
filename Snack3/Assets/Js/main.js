5a233bbf8e4b0637ddaa7945104e9eade3b1e043
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-18 19:57:11 +0200

    fix js file Snack3 pulito il codice con aggiunta ciclo for x 10 elementi chiesti all'utente

--- Snack3/Assets/Js/main.js
+++ Snack3/Assets/Js/main.js
@@ -3,36 +3,36 @@
 /* Strumenti
 -prompt
 -querySelector
--if/else
+-for
 */
 
 // inizializzazione variabili
-const numberOne = Number(prompt("Inserisci un numero"));
-const numberTwo = Number(prompt("Inserisci un altro numero"));
-const numberThree = Number(prompt("Inserisci un altro numero"));
-const numberFour = Number(prompt("Inserisci un altro numero"));
-const numberFive = Number(prompt("Inserisci un altro numero"));
-const numberSix = Number(prompt("Inserisci un altro numero"));
-const numberSeven = Number(prompt("Inserisci un altro numero"));
-const numberEight = Number(prompt("Inserisci un altro numero"));
-const numberNine = Number(prompt("Inserisci un altro numero"));
-const numberTen = Number(prompt("Inserisci un altro numero"));
+let sumNumbers = 0;
+let message = false;
 
-const sumNumbers = numberOne + numberTwo + numberThree + numberFour + numberFive + numberSix + numberSeven + numberEight + numberNine + numberTen;
+// ciclo for
+for (let index = 0; index < 10; index++) {
+    let askNumber = Number(prompt("Inserisci un numero"));
+    /* console.log(askNumber); */
+   
+    sumNumbers = sumNumbers + askNumber;
+    /* console.log(sumNumbers); */
 
-
-
-// condizione di verifica
-if (isNaN(numberOne) || isNaN(numberTwo) || isNaN(numberThree) || isNaN(numberFour) || isNaN(numberFive) || isNaN(numberSix) || isNaN(numberSeven) || isNaN(numberEight)
-|| isNaN(numberNine) || isNaN(numberTen)
-) {
-
-     let message = `si prega di indicare un numero corretto`;
-    document.querySelector("h1").innerHTML = message;
-
-} else {
-    //stampa a schermo
-document.querySelector("h1").innerHTML = `la somma dei numeri digitati è: ${sumNumbers}`;
 }
+
+
+//stampa a schermo
+document.querySelector("h1").innerHTML = `la somma dei numeri digitati è: ${sumNumbers}`;
+ 
+
+
+
+
+
+
+
+    
+
+
 
 
