c111b424ffca2adb4ec5658aa669500b7ba426f1
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-18 15:22:10 +0200

    complete details js file
    aggiunto prompt e condizione di verifica

--- Snack1/Assets/Js/main.js
+++ Snack1/Assets/Js/main.js
@@ -5,3 +5,27 @@
 -if/else
 -querySelector
 */
+
+// inizializzazione variabili
+const numberOne = Number(prompt("Inserisci un numero"));
+const numberTwo = Number(prompt("Inserisci un altro numero"));
+
+// condizione di verifica
+if (isNaN(numberOne) || isNaN(numberTwo)) {
+     let message = `si prega di indicare un numero corretto`;
+    document.querySelector("h1").innerHTML = message;
+
+}
+else if (numberOne > numberTwo) {
+    let message = `il numero ${numberOne} è maggiore del numero ${numberTwo}`;
+    document.querySelector("h1").innerHTML = message;
+    /* console.log(`il numero ${numberOne} è maggiore del numero ${numberTwo}`); */
+} else if (numberOne < numberTwo) {
+    let message = `il numero ${numberTwo} è maggiore del numero ${numberOne}`;
+    document.querySelector("h1").innerHTML = message;
+    /* console.log(`il numero ${numberTwo} è maggiore del numero ${numberOne}`); */
+} else {
+    let message = `i numeri digitati sono identici`;
+    document.querySelector("h1").innerHTML = message;
+   /*  console.log(`i numeri digitati sono identici`); */
+}
