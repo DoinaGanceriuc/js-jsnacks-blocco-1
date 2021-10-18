898a4f5a02e2bf1e6337dca4315fc399eb848604
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-18 16:54:58 +0200

    complete details js file Snack4
    aggiunto prompt, ciclo for e condizionale

--- Snack4/Assets/Js/main.js
+++ Snack4/Assets/Js/main.js
@@ -2,8 +2,36 @@
 no alla festa. */
 
 /* Strumenti
+-array
 -prompt
 -for
 -if/else
 -querySelector/getElementById
 */
+
+// inizializzazione variabili
+const guests = ["Luca", "Anna", "Sara", "Marco", "Filippo", "Lucia", "Stefano", "Lorenzo", "Francesca", "Ludovico"];
+
+const userName = prompt("Quale è il tuo nome?")
+const userNameDigit = userName[0].toUpperCase() + userName.slice(1);
+/* console.log(userNameDigit); */
+let access = false;
+
+
+// ciclo for
+for (let i = 0; i < guests.length; i++) {
+    const singleUser = guests[i];
+    /* console.log(singleUser); */
+
+    if (userNameDigit == singleUser) {
+    access = true;
+   
+}
+}
+// condizione di verifica
+if (access == true) {
+    document.getElementById("one").innerHTML = `${userNameDigit} puoi entrare`;
+
+} else {
+   document.getElementById("one").innerHTML = `${userNameDigit} NON puoi entrare`; 
+}
